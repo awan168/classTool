1a0554a9128d6f79b052c6a02241da79d7a1cb6a
TATUNG LIN <ttl.168@gmail.com>
2023-04-01 23:56:24 -0700

    04012023_initialCheckIn

---
+++ CEU_TC_肺系/header.js
@@ -0,0 +1,17 @@
+document.write("<title>呼吸系統疾病臨床方劑與針灸的應用</title>    <center><img src=\"top_title.png\" width=\"55%\"> \n\
+    <br> \n\
+    <button onclick=\"location.href='cate_感冒常用方劑.html'\" type=\"button\">感冒常用方劑</button> \n\
+    <button onclick=\"location.href='cate_太陽病三大方劑.html'\" type=\"button\">太陽病三大方劑</button> \n\
+    <button onclick=\"location.href='cate_葛根湯類方.html'\" type=\"button\">葛根湯類方</button> \n\
+    <button onclick=\"location.href='cate_小青龍湯類方.html'\" type=\"button\">小青龍湯類方</button> \n\
+    <button onclick=\"location.href='cate_更進一步的感冒方劑.html'\" type=\"button\">更進一步的感冒方劑</button> \n\
+    <button onclick=\"location.href='cate_五臟咳嗽證治心要一覽.html'\" type=\"button\">五臟咳嗽證治心要一覽</button> \n\
+    <button onclick=\"location.href='cate_咳嗽.html'\" type=\"button\">咳嗽</button> \n\
+    <button onclick=\"location.href='cate_氣喘.html'\" type=\"button\">氣喘</button> \n\
+    <button onclick=\"location.href='cate_呼吸困難短氣.html'\" type=\"button\">呼吸困難短氣</button> \n\
+    <button onclick=\"location.href='cate_感冒重要穴組.html'\" type=\"button\">針灸: 感冒重要穴組</button> \n\
+    <button onclick=\"location.href='cate_名家用穴.html'\" type=\"button\">針灸: 大師針灸取穴</button> \n\
+    </center> \n\
+    <br> \n\
+    <br>      \n\
+")
