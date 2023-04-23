f1b063422fce571106c5aaf95edb25e5112da196
TATUNG LIN <ttl.168@gmail.com>
2023-04-22 19:57:32 -0700

    Add files via upload

---
+++ books/header.js
@@ -0,0 +1,18 @@
+document.write("<title>林大棟醫師著作美國購書專頁</title>    <center><img src=\"top_title.png\" width=\"55%\"> \n\
+    <br> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_books.html'\" type=\"button\">首頁</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b0.html'\" type=\"button\">佛州漢唐跟診日誌：師從倪海廈先生見聞錄</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_ba.html'\" type=\"button\">藥香中尋找愛(原創小說)</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_bb.html'\" type=\"button\">馬雅神教教主本紀：少年王比利的故事(原創小說)</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b1.html'\" type=\"button\">經穴春秋</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b2.html'\" type=\"button\">藥食心源---中醫養生飲食總整理</button> \n\
+    <br>\n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b3.html'\" type=\"button\">扶陽之祖_大宋竇材的扁鵲心書</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b4.html'\" type=\"button\">中醫夢紅樓：大觀園女子健康診斷書</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b5.html'\" type=\"button\">中醫超級兒童私房課</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b6.html'\" type=\"button\">AI岐黃——中醫大腦醫案集</button> \n\
+    <button style='background-color: #ccffcc; font-size:20px' onclick=\"location.href='top_b7.html'\" type=\"button\">AI岐黃——中醫大腦重症醫案集</button> \n\
+    </center> \n\
+    <br> \n\
+    <br>      \n\
+")
