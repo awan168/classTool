a58438ec5d8b80f8b868c4b383b3f0e133c6f8b6
TATUNG LIN <ttl.168@gmail.com>
2023-05-13 15:27:42 -0700

    Add files via upload

---
+++ DrLinClass/header.js
@@ -0,0 +1,12 @@
+document.write("<title>林建樑老師課程</title>    <center><img src=\"top_title.png\" width=\"55%\"> \n\
+<script> function goBack() {  window.history.back(); } </script><a name='home'></a><div style=\"position : fixed;right:5px\"><button onclick=\"location.href='#home'\">回首頁</button><br><button onclick=\"goBack()\">回前頁</button></div>  \n\
+    <br> \n\
+    <button onclick=\"location.href='top_MI.html'\" type=\"button\">首頁</button> \n\
+    <button onclick=\"location.href='cate_01_summary.html'\" type=\"button\">月經之正常</button> \n\
+    <button onclick=\"location.href='cate_02_summary.html'\" type=\"button\">月經之規則</button> \n\
+    <button onclick=\"location.href='cate_03_short.html'\" type=\"button\">月經先期說明</button> \n\
+    <button onclick=\"location.href='cate_04_long.html'\" type=\"button\">月經後期說明</button> \n\
+    <button onclick=\"location.href='cate_05_shortHeal.html'\" type=\"button\">月經先期治療</button> \n\
+    <button onclick=\"location.href='cate_06_longHeal.html'\" type=\"button\">月經後期治療</button> \n\
+    </center>   \n\
+")
