3fff8779b0c8ce8ffe56a7376d713b7fb6eda563
TATUNG LIN <ttl.168@gmail.com>
2023-06-03 23:41:54 -0700

    Add files via upload

---
+++ DrLinClass/tongueDiagnosis/header.js
@@ -0,0 +1,15 @@
+document.write("<title>舌診心要總整理</title>    <center><img src=\"top_title.png\" width=\"55%\"> \n\
+    <br> \n\
+    <button onclick=\"location.href='top_CEU_舌診心要.html'\" type=\"button\">首頁</button> \n\
+    <button onclick=\"location.href='cate_總論.html'\" type=\"button\">總論</button> \n\
+    <button onclick=\"location.href='cate_舌質.html'\" type=\"button\">舌質</button> \n\
+    <button onclick=\"location.href='cate_舌苔.html'\" type=\"button\">舌苔</button> \n\
+    <button onclick=\"location.href='cate_舌形舌象.html'\" type=\"button\">舌形舌象</button> \n\
+    <button onclick=\"location.href='cate_舌診與八綱辨證.html'\" type=\"button\">舌診與八綱辨證</button> \n\
+    <button onclick=\"location.href='cate_西藥對舌象的影響.html'\" type=\"button\">西藥對舌象的影響</button> \n\
+    <button onclick=\"location.href='cate_陰陽虛實之舌診心要.html'\" type=\"button\">陰陽虛實之舌診心要</button> \n\
+    <button style=\"background-color: #ccffcc;\" onclick=\"location.href='cate_舌診用方一覽.html'\" type=\"button\">舌診用方一覽</button> \n\
+    </center> \n\
+    <br> \n\
+    <br>      \n\
+")
