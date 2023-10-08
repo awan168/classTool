3fec4f8ee17ebfe0070e3f9812c11af91ebd0f77
TATUNG LIN <ttl.168@gmail.com>
2023-10-07 22:39:54 -0700

    Add files via upload

---
+++ dysk_大醫小課/myAcup_必備針灸取穴與決勝針法/acupElement.js
@@ -0,0 +1,135 @@
+let    HandTaiYing  = "☱ 1 手太陰肺經";
+let    HandShaoYing = "☲ 1 手少陰心經";
+let    HandJueYing  = "☳ 1 手厥陰心包經";
+let    HandTaiYang  = "☲ 0 手太陽小腸經";
+let    HandShaoYang = "☳ 0 手少陽三焦經";
+let    HandYangMing = "☱ 0 手陽明大腸經";
+let    FootTaiYing  = "☶ 1 足太陰脾經";
+let    FootShaoYing = "☵ 1 足少陰腎經";
+let    FootJueYing  = "☴ 1 足厥陰肝經";
+let    FootTaiYang  = "☵ 0 足太陽膀胱經";
+let    FootShaoYang = "☴ 1 足少陽膽經";
+let    FootYangMing = "☶ 0 足陽明胃經"; 
+
+let    SuP_00_1="少商";
+let    Sup_00_2="魚際";
+let    Sup_00_3="太淵";
+let    Sup_00_4="列缺";
+let    Sup_00_5="經渠";
+let    Sup_00_6="尺澤";
+let    Sup_01_1="商陽";
+let    Sup_01_2="二間";
+let    Sup_01_3="三間";
+let    Sup_01_4="合谷";
+let    Sup_01_5="陽谿";
+let    Sup_01_6="曲池";
+let    Sup_02_1="厲兌";
+let    Sup_02_2="內庭";
+let    Sup_02_3="陷谷";
+let    Sup_02_4="衝陽";
+let    Sup_02_5="解谿";
+let    Sup_02_6="足三里";
+let    Sup_03_1="隱白";
+let    Sup_03_2="大都";
+let    Sup_03_3="太白";
+let    Sup_03_4="公孫";
+let    Sup_03_5="商丘";
+let    Sup_03_6="陰陵泉";
+let    Sup_04_1="少衝";
+let    Sup_04_2="少府";
+let    Sup_04_3="神門";
+let    Sup_04_4="通里";
+let    Sup_04_5="靈道";
+let    Sup_04_6="少海";
+let    Sup_05_1="少澤";
+let    Sup_05_2="前谷";
+let    Sup_05_3="後谿";
+let    Sup_05_4="腕骨";
+let    Sup_05_5="陽谷";
+let    Sup_05_6="小海";
+let    Sup_06_1="至陰";
+let    Sup_06_2="足通谷";
+let    Sup_06_3="束骨";
+let    Sup_06_4="京骨";
+let    Sup_06_5="崑崙";
+let    Sup_06_6="委中";
+let    Sup_07_1="涌泉";
+let    Sup_07_2="然谷";
+let    Sup_07_3="太谿";
+let    Sup_07_4="大鐘";
+let    Sup_07_5="復溜";
+let    Sup_07_6="陰谷";
+let    Sup_08_1="中衝";
+let    Sup_08_2="勞宮";
+let    Sup_08_3="大陵";
+let    Sup_08_4="內關";
+let    Sup_08_5="間使";
+let    Sup_08_6="曲澤";
+let    Sup_09_1="關衝";
+let    Sup_09_2="液門";
+let    Sup_09_3="中渚";
+let    Sup_09_4="陽池";
+let    Sup_09_5="支溝";
+let    Sup_09_6="天井";
+let    Sup_10_1="足竅陰";
+let    Sup_10_2="俠谿";
+let    Sup_10_3="足臨泣";
+let    Sup_10_4="丘墟";
+let    Sup_10_5="陽輔";
+let    Sup_10_6="陽陵泉";
+let    Sup_11_1="大敦";
+let    Sup_11_2="行間";
+let    Sup_11_3="太衝";
+let    Sup_11_4="蠡溝";
+let    Sup_11_5="中封";
+let    Sup_11_6="曲泉";
+
+let    SuPList_00 = ["太淵", "尺澤", "太白", "陰谷", Sup_00_3, Sup_00_6, "太淵", "偏歷"] ; // 肺  
+let    SuPList_01 = ["曲池", "二間", "三里", "通谷", Sup_01_3, Sup_01_6, "合谷", "列缺"] ; // 大腸
+let    SuPList_02 = ["解谿", "厲兌", "陽谷", "商陽", Sup_02_3, Sup_02_6, "衝陽", "公孫"] ; // 胃  
+let    SuPList_03 = ["大都", "商丘", "少府", "經渠", Sup_03_3, Sup_03_6, "太白", "豐隆"] ; // 脾  
+let    SuPList_04 = ["少沖", "神門", "大敦", "太白", Sup_04_3, Sup_04_6, "神門", "支正"] ; // 心  
+let    SuPList_05 = ["後谿", "小海", "臨泣", "三里", Sup_05_3, Sup_05_6, "腕骨", "通里"] ; // 小腸
+let    SuPList_06 = ["至陰", "束骨", "商陽", "臨泣", Sup_06_3, Sup_06_6, "京骨", "大鐘"] ; // 膀胱
+let    SuPList_07 = ["復溜", "湧泉", "經渠", "大敦", Sup_07_3, Sup_07_6, "太谿", "飛揚"] ; // 腎  
+let    SuPList_08 = ["中沖", "大陵", "大敦", "太白", Sup_08_3, Sup_08_6, "大陵", "外關"] ; // 心包
+let    SuPList_09 = ["中渚", "天井", "臨泣", "三里", Sup_09_3, Sup_09_6, "陽池", "內關"] ; // 三焦
+let    SuPList_10 = ["俠谿", "陽輔", "通谷", "陽谷", Sup_10_3, Sup_10_6, "丘墟", "蠡溝"] ; // 膽  
+let    SuPList_11 = ["曲泉", "行間", "陰谷", "少府", Sup_11_3, Sup_11_6, "太衝", "光明"] ; // 肝  
+
+
+
+let organDiag_LU = "<br>肺(LU): <br>    [經絡所行]: 掌心發熱、肩內側痛、兩乳痛、胸脅滿痛 <br>    [臟象表現]: 汗出、痰多、氣管發炎、咽痛、氣短、鼻淵、鼻塞流涕、咳喘";
+
+let organDiag_LI = "<br>大腸(LI): <br>    [經絡所行]: 牙齦發炎、牙痛、腮腺炎、咽喉炎、頷下淋巴腺炎、口乾、目黃、鼻流清涕、肩前側痛 <br>    [臟象表現]: 便溏下利、腸鳴、大便臭穢、便膿血、便祕";
+
+let organDiag_ST = "<br>胃(ST): <br>    [經絡所行]: 鼻出血、面神經麻痺、下肢前側痛、膝關節酸痛 <br>    [臟象表現]: 喉嚨痛、胃痛、怕熱、消化不良、倦怠、便祕、唇乾舌燥、消瘦";
+
+let organDiag_SP = "<br>脾(SP): <br>    [經絡所行]: 舌強、腹痛、下肢內側痛 <br>    [臟象表現]: 身體沈重感、腹脘脹氣、吸收不良、口淡、嘔吐悶脹、頭脹不清、濕重腳腫、便溏、關節酸痛";
+
+let organDiag_HT = "<br>心(HT): <br>    [經絡所行]: 上肢尺側痛、狐臭 <br>    [臟象表現]: 心煩、心驚、心悸、心悶、心痛、短氣、易怒、憂鬱、口腔潰瘍、口乾、口臭";
+
+
+let organDiag_SI = "<br>小腸(SI): <br>    [經絡所行]: 下頷腫痛、肩痛、肩周炎、頸痛、小腹繞臍痛、耳聾、上肢外後側痛 <br>    [臟象表現]: 腹瀉、手足寒、吸收不良、虛胖";
+
+
+let organDiag_BL = "<br>膀胱(BL): <br>    [經絡所行]: 眼痛、頸痛、腰背痛、下肢痛、角弓反張、眶上神經痛、鼻出血、脫肛、痔瘡 <br>    [臟象表現]: 外感、尿頻、尿多、前列腺肥大";
+
+
+let organDiag_KI = "<br>腎(KI): <br>    [經絡所行]: 腰酸痛、下肢無力、喉嚨痛 <br>    [臟象表現]: 神經衰弱、精神不振、食慾不佳、視力減退、面色灰暗、咽喉炎、月經不調、性慾減退、前列腺肥大、足跟痛、尿頻、尿少、手足寒";
+
+
+let organDiag_PC = "<br>心包(PC): <br>    [經絡所行]: 前臂及手指痙孿疼痛、胸肋痛、心區痛、掌心發熱 <br>    [臟象表現]: 心悸、心煩、喜笑不休、失眠多夢、易醒、難入眠、健忘";
+
+
+let organDiag_SJ = "<br>三焦(SJ): <br>    [經絡所行]: 咽喉痛、耳聾、耳鳴、結膜炎、肩背痛、脊間痛 <br>    [臟象表現]: 皮膚過敏、肌肉關節酸痛無力、手足怕冷、上熱下寒、食慾不振";
+
+
+let organDiag_GB = "<br>膽(GB): <br>    [經絡所行]: 脅肋痛、偏頭痛、眼痛、頸淋巴性結節、甲狀腺腫 <br>    [臟象表現]: 易驚、口苦、咽乾、善嘆息、消化不良、關節痛、脂肪瘤、痰濕結節積聚";
+
+
+let organDiag_LR = "<br>肝(LR): <br>    [經絡所行]: 胸脅苦滿、腹痛、睪丸炎、疝氣、前列腺肥大 <br>    [臟象表現]: 口乾、口苦、情志抑鬱、小便不利、眩暈、血壓不穩、易怒、月經不調、乳房疾病 皮膚萎黃、倦怠";
+
+
+let    orgDiagList = [organDiag_LU, organDiag_LI, organDiag_ST, organDiag_SP, organDiag_HT, organDiag_SI, organDiag_BL, organDiag_KI, organDiag_PC, organDiag_SJ, organDiag_GB, organDiag_LR  ];
+     
---
+++ dysk_大醫小課/myAcup_必備針灸取穴與決勝針法/header.js
@@ -0,0 +1,19 @@
+document.write("<title>精一書院:現代針灸</title><a name='home'></a>    <center><img src=\"top_title.png\" width=\"55%\"> \n\
+<script> function goBack() {  window.history.back(); } </script><div style=\"position : fixed;right:5px\"><button onclick=\"location.href='#home'\">回首頁</button><br><button onclick=\"goBack()\">回前頁</button></div>  \n\
+    <br> \n\
+    <button onclick=\"location.href='top_ma_現代針灸的大整理及查找工具.html'\" type=\"button\">首頁</button> \n\
+    <button onclick=\"location.href='cate_acup3Ways.html'\" type=\"button\">三大針灸取穴方向</button> \n\
+    <button onclick=\"location.href='cate_transportPointTool.html'\" type=\"button\">特定穴的取穴</button> \n\
+    <button onclick=\"location.href='cate_接經療法.html'\" type=\"button\">接經療法</button> \n\
+    <button onclick=\"location.href='cate_頭針大整理.html'\" type=\"button\">頭針總整理</button> \n\
+    <button onclick=\"location.href='cate_頭針擴大運用.html'\" type=\"button\">頭針擴大運用</button> \n\
+    <button onclick=\"location.href='cate_頭針臨床個論.html'\" type=\"button\">頭針臨床個論</button> \n\
+    <button onclick=\"location.href='cate_腹針大整理.html'\" type=\"button\">腹針總整理</button> \n\
+    <button onclick=\"location.href='cate_耳穴大整理.html'\" type=\"button\">耳穴總整理</button> \n\
+    <button onclick=\"location.href='cate_手穴.html'\" type=\"button\">手穴總整理</button> \n\
+    <button onclick=\"location.href='cate_足穴整理.html'\" type=\"button\">足穴整理</button> \n\
+    <button onclick=\"location.href='cate_自律神經針法.html'\" type=\"button\">自律神經針法</button> \n\
+    <button onclick=\"location.href='cate_總訣式.html'\" type=\"button\">總訣式</button> \n\
+    </center> \n\
+    <br>      \n\
+")
