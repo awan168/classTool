40831786828b2cba7d3f2c09fc8d97e2e5cf40b5
TATUNG LIN <ttl.168@gmail.com>
2023-12-03 22:50:05 -0800

    Add files via upload

---
+++ dysk_大醫小課/ health_問止養生律/header.js
@@ -0,0 +1,3 @@
+document.write("<script> function goBack() {  window.history.back(); } </script><a name='home'></a><div style=\"position : fixed;right:5px\"><button onclick=\"location.href='#home'\">回首頁</button><br><button onclick=\"goBack()\">回前頁</button></div>  \n\
+    <br>      \n\
+")
